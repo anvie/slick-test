b3cd3271e8730e7ac5c93867db52fd6abfed2992
robin <robin@ansvia.com>
2014-08-16 22:05:44 +0700

    prepare automated share scheduler, wiki, api doc.

---
+++ core/src/main/scala/com/ansvia/zufaro/live/ShareScheduler.scala
@@ -0,0 +1,19 @@
+package com.ansvia.zufaro.live
+
+/**
+ * Author: robin
+ * Date: 8/16/14
+ * Time: 9:57 PM
+ *
+ */
+
+/**
+ * Used for automated processing profit share.
+ */
+class ShareScheduler extends Thread {
+    override def run() {
+        // @TODO(robin): code here
+    }
+}
+
+
